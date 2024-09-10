de0cc63f5a81ab687cab48428185d044764bae6e
Valencia <156272281+kyushu01@users.noreply.github.com>
2024-09-10 14:01:55 +0800

    Delete Conversion directory

--- Conversion/script.js
+++
@@ -1,9 +0,0 @@
-function convertHPtoKW() {
-    const hpValue = document.getElementById('hpValue').value;
-    if (hpValue) {
-        const kwValue = (hpValue * 0.735499).toFixed(2);
-        document.getElementById('result').textContent = `${hpValue} HP is equal to ${kwValue} KW.`;
-    } else {
-        document.getElementById('result').textContent = "Please enter a valid HP value.";
-    }
-}

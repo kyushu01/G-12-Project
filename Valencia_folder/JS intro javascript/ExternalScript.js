cdeffcb61b578deffbbce93a010a52c17a3fd409
Valencia <156272281+kyushu01@users.noreply.github.com>
2024-11-06 02:30:44 +0800

    Delete Valencia_folder directory

--- Valencia_folder/JS intro javascript/ExternalScript.js
+++
@@ -1,8 +0,0 @@
-function myGreeting(){
-    alert("Hello JavaScript!");   
-}
-function showAlert(){
-    alert("Please click OK to continue.")
-    alert("Please click OK to again.")
-    alert("Click OK for the last time to continue.")
-}
